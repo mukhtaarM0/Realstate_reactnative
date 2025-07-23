0b5216ea1a6b6142579141cfb8614dd7e079cae6
Magacaaga <emailkaaga@example.com>
2025-07-23 01:20:57 -0700

    signup user update

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -25,7 +25,7 @@ const RegistrationForm = () => {
     console.log('Submitted', values);
 
     try {
-      const response = await axios.post(`${process.env.REACT_APP_API_URL}/user`, values);
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/users`, values);
       console.log('Server Response:', response.data); // Waan ku daray tan
       alert(`Welcome: ${response.data.message}`);
     } catch (error) {
